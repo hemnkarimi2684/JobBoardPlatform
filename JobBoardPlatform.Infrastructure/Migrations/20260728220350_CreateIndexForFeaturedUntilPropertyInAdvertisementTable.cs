3cb4125ef77d4998e1e479a990d27ab7828dee8d
Hemen Karimi <hemnkarimi@gmail.com>
2026-07-29 01:34:53 +0330

    Create Index For FeaturedUntil Property In Advertisement Table

---
+++ JobBoardPlatform.Infrastructure/Migrations/20260728220350_CreateIndexForFeaturedUntilPropertyInAdvertisementTable.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobBoardPlatform.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIndexForFeaturedUntilPropertyInAdvertisementTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisements_FeaturedUntil",
+                table: "Advertisements",
+                column: "FeaturedUntil");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Advertisements_FeaturedUntil",
+                table: "Advertisements");
+        }
+    }
+}
